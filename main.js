793e40e9120fd84e464634677b795c77cbafc53a
lamericaindotcom <pierre-ledez@hotmail.fr>
2025-10-31 11:31:04 +0100

    eval en cours

---
+++ main.js
@@ -0,0 +1,45 @@
+// Heure afficher
+function showTime() {
+    const timeElement = document.getElementById('heure-display');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(showTime, 1000);
+showTime(); // Afficher immédiatement au chargement de la page
+
+
+//Ma tache à faire
+
+localStorage.setItem('tache', 'Faire le café');
+
+const tacheElement = document.getElementById('tache-display');
+const tache = localStorage.getItem('tache');
+tacheElement.textContent = tache ? tache : 'Aucune tâche enregistrée';
+
+
+
+
+
+
+// Météo Toulouse
+
+fetch('https://prevision-meteo.ch/services/json/toulouse')
+    .then(response => response.json())
+    .then(data => {
+        
+        const temperature = data.current_condition.tmp;
+        const condition = data.current_condition.condition;
+        const vitesseVent = data.current_condition.wnd_spd;
+        const ventElement = document.getElementById('meteo-display');
+        ventElement.textContent = `Vent: ${vitesseVent} km/h, Température: ${temperature} °C, Condition: ${condition}`;
+    
+    })
+    .catch(error => {
+        console.error('Erreur lors de la récupération des données météo :', error);
+        const messageErreur = document.getElementById('meteo-display');
+        messageErreur.textContent = 'Impossible de récupérer les données météo.';
+    });  
